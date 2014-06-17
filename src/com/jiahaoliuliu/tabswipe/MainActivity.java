e9df9c10d9ba38bcc2b2d3546483a04439f9e2e2
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-06-17 18:21:39 +0200

    First commit

---
+++ src/com/jiahaoliuliu/tabswipe/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jiahaoliuliu.tabswipe;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
