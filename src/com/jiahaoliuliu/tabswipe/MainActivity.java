c1737d0e3058da64a5517d4069c3fd85e4bf4a76
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-06-17 18:59:35 +0200

    Drawer over Tabs. First try

--- src/com/jiahaoliuliu/tabswipe/MainActivity.java
+++ src/com/jiahaoliuliu/tabswipe/MainActivity.java
@@ -2,16 +2,26 @@ package com.jiahaoliuliu.tabswipe;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.Tab;
-import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends SherlockFragmentActivity implements
-	ActionBar.TabListener{
+	ActionBar.TabListener {
 
+	private DrawerLayout mDrawerLayout;
+	private LinearLayout mDrawer;
+	private ActionBarDrawerToggle mDrawerToggle;
+	
 	private ViewPager viewPager;
 	private TabsPagerAdapter mAdapter;
 	private ActionBar actionBar;
@@ -23,6 +33,34 @@ public class MainActivity extends SherlockFragmentActivity implements
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Drawer
+		mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+		mDrawer = (LinearLayout)findViewById(R.id.drawer);
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+		getSupportActionBar().setHomeButtonEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		
+		mDrawerToggle = new ActionBarDrawerToggle(
+				this,
+				mDrawerLayout,
+				R.drawable.ic_drawer,
+				R.string.drawer_open,
+				R.string.drawer_close) {
+			
+			public void onDrawerClosed(View view) {
+				super.onDrawerClosed(view);
+			}
+			
+			public void onDrawerOpened(View drawerView) {
+				// Set the title on the action when drawer open
+				getSupportActionBar().setTitle("Title");
+				super.onDrawerOpened(drawerView);
+			}
+		};
+		
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
 		
 		// Initialization
 		viewPager = (ViewPager) findViewById(R.id.pager);
@@ -30,7 +68,6 @@ public class MainActivity extends SherlockFragmentActivity implements
 		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
 		
 		viewPager.setAdapter(mAdapter);
-		actionBar.setHomeButtonEnabled(false);
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		
 		for (String tabName: tabs) {
@@ -72,5 +109,31 @@ public class MainActivity extends SherlockFragmentActivity implements
 	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			if (mDrawerLayout.isDrawerOpen(mDrawer)) {
+				mDrawerLayout.closeDrawer(mDrawer);
+			} else {
+				mDrawerLayout.openDrawer(mDrawer);
+			}
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		mDrawerToggle.syncState();
+	}
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggles
+		mDrawerToggle.onConfigurationChanged(newConfig);
 	}
 }
